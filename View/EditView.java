76ad7a9b5a37e41a00f38f2cfa8fa1d5fd5384ba
icebear2n2 <icebear2n2@gmail.com>
2023-06-12 09:53:48 +0900

    개발자 모드 view 완료

--- View/EditView.java
+++ View/EditView.java
@@ -1,15 +1,8 @@
 package View;
 
 public class EditView {
-    public static void printeditView() {
-        System.out.println("에딧 모듈 입니다");
-        System.out.println("1. 비행기 조회  2. 비행기 추가  3. 비행기 삭제 4. 유저 정보 조회 5. 유저 정보 추가 6.유저 정보 삭제 0. 종료");
-    }
 
-    public void printOutLineDevelop() {
-
-    }
-
+    // 개발자 모드 메인 메뉴
     public void printDevelopMenu() {
         System.out.println("=====================[ 개발자 모드 ]=====================");
         System.out.println("1. 비행기 정보 조회  2. 비행기 추가  3. 비행기 삭제");
@@ -18,6 +11,7 @@ public class EditView {
         System.out.println("번호 입력 >> ");
     }
 
+    // 비행기 정보 입력 받기
     public void printAirplaneInfo(int num) {
         if (num == 1) {
             System.out.println("비행기 이름 입력 >> ");
@@ -25,6 +19,19 @@ public class EditView {
             System.out.println("출발지 입력 >> ");
         } else if (num == 3) {
             System.out.println("도착지 입력 >> ");
+        }
+    }
+
+    // 유저 정보 입력 받기
+    public void printUserInfo(int num) {
+        if (num == 1) {
+            System.out.println("아이디 입력 >> ");
+        }
+        else if (num == 2) {
+            System.out.println("패스워드 입력 >> ");
+        }
+        else if (num == 3) {
+            System.out.println("이름 입력 >> ");
         }
     }
 
