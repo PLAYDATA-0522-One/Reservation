01dc2c0888275130c7a94627aea4893ba027dbcc
icebear2n2 <icebear2n2@gmail.com>
2023-06-19 09:13:27 +0900

    예약 화면 추가 구현

--- View/ReservationView.java
+++ View/ReservationView.java
@@ -2,7 +2,7 @@ package View;
 
 public class ReservationView {
 
-    public void printOutLineAirplane(){
+    public void printOutLineAirplane() {
         System.out.println("========================");
     }
 
@@ -13,20 +13,32 @@ public class ReservationView {
         System.out.println("1. 조회  2. 예약 3. 티켓 확인  0.이전 메뉴");
     }
 
-    public void printReservationAirPlan_askShowList(){
+    public void printReservationAirPlan_askShowList() {
         System.out.println("비행기 목록을 확인 하시겠습니까?");
         System.out.println("1. 확인  2. 바로 예약");
     }
 
-    public void printInputAirPlanName(){
+    public void printInputAirPlanName() {
         System.out.println("비행기 이름을 입력해 주세요.");
     }
 
-    public void printSuccessAirPlane(){
+    public void printInputSeatName() {
+        System.out.println("예약할 좌석 이름을 입력해 주세요.");
+    }
+
+    public void printSeatList() {
+        System.out.println("==================[전체 좌석 리스트]==================");
+    }
+
+    public void printReservationSeatList() {
+        System.out.println("==================[예약 불가 좌석 리스트]==================");
+    }
+
+    public void printSuccessAirPlane() {
         System.out.println("예약이 성공적으로 완료 되었습니다.");
     }
 
-    public void printFindAirplaneError(){
+    public void printFindAirplaneError() {
         System.out.println("비행기를 찾을 수 없습니다.");
     }
 
